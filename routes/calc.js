bf44543635c7dbf71043fb58848d0e2c09160e34
TereseBo <terese.boderus@gmail.com>
2023-05-12 14:28:38 +0200

    Update of pre-commit hook, calc endponts and calculator

--- routes/calc.js
+++ routes/calc.js
@@ -4,7 +4,21 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('index', { title: 'Calc' });
+  res.render('index', { title: 'Calc', text: '' });
+});
+
+router.get('/add', (req, res, next) => {
+  res.render('calc', { title: 'Add', text: '' });
+});
+router.get('/subtract', (req, res, next) => {
+  res.render('calc', { title: 'Sub', text: '' });
+});
+router.get('/multiply', (req, res, next) => {
+  res.render('calc', { title: 'Mult', text: '' });
+});
+
+router.get('/divide', (req, res, next) => {
+  res.render('calc', { title: 'Div', text: '' });
 });
 
 module.exports = router;

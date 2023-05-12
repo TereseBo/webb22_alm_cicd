1f2a17f422ab8f4bd37b104d11673711f401320a
TereseBo <terese.boderus@gmail.com>
2023-05-12 08:11:13 +0200

    First commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Pandemonium', img:["https://images.unsplash.com/photo-1527118732049-c88155f2107c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"] });
+});
+
+module.exports = router;

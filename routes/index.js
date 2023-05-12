8937235340eef9b5e6f3127e5a01e87bb2b17366
TereseBo <terese.boderus@gmail.com>
2023-05-12 14:27:05 +0200

    Restructure of render templates

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Pandemonium', img: ['https://images.unsplash.com/photo-1527118732049-c88155f2107c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'] });
+  res.render('index', { title: 'Pandemonium', text: 'Welcome to our fabulous group-page' });
 });
 
 module.exports = router;
